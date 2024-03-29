e8f07a13dc8cef3436b4d0a7f754637c634efb33
JCaeta <63880187+JCaeta@users.noreply.github.com>
2024-03-29 19:29:51 -0300

    Agregar grid para botones

--- tp2/controller.py
+++ tp2/controller.py
@@ -1,35 +1,44 @@
-from ui_utils import PyQtApp
+from ui import PyQtApp
 from generators import normalDist
 from generators import mixedCongruentGenerator
 
 class Controller:
     def __init__(self):
         self.ui = PyQtApp() # Es la clase que hace toda la parte gráfica
-        self.create_buttons()
+        self.build_interface()
     
-    def create_buttons(self):
-        button_width = 100
-        self.ui.create_button(
-            'Dist Uniforme', 
-            self.create_uniform_distribution, 
-            10,
-            10)
+    def on_change_a(self, value):
+        print('a: ', value)
         
-        self.ui.create_button(
-            'Dist Exponencial', 
-            self.create_normal_distribution, 
-            10 + button_width,
-            10)
+    def on_change_b(self, value):
+        print('b: ', value)
         
-        self.ui.create_button(
-            'Dist Normal', 
-            self.create_normal_distribution, 
-            10 + button_width*2,
-            10)
+    def on_change_sample_size(self, value):
+        print('b: ', value)
+        
+    def on_change_intervals(self, value):
+        print('b: ', value)
     
     def create_uniform_distribution(self):
         print("create_uniform_distribution")
-        # self.ui.plot_chart()
+    
+    def build_interface(self):
+        self.ui.create_button(
+            'Dist Uniforme', 
+            self.create_uniform_distribution)
+        
+        self.ui.create_button(
+            'Dist Exponencial', 
+            self.create_exponential_distribution)
+        
+        self.ui.create_button(
+            'Dist Normal', 
+            self.create_normal_distribution)
+        
+        self.a_input = self.ui.create_input('a', self.on_change_a)
+        self.b_input = self.ui.create_input('b', self.on_change_b)
+        self.sample_size_input = self.ui.create_input('Tamaño de muestra', self.on_change_sample_size, width = 120)
+        self.interval_input = self.ui.create_input('Intervalos', self.on_change_intervals, width = 120)
         
     def create_normal_distribution(self):
         # Constants 
@@ -60,6 +69,14 @@ class Controller:
         }
         
         self.ui.plot_histogram_chart(chart_params)
+
+    def create_exponential_distribution(self):
+        print("create_exponential_distribution()")
+        # Retrieve and print the input values
+        print("a input:", self.a_input.text())
+        print("b input:", self.b_input.text())
+        print("Sample size input:", self.sample_size_input.text())
+        print("Interval input:", self.interval_input.text())
 
     def start(self):
         self.ui.run()
