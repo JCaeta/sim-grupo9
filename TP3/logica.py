4c26548c92d7f7408c2941f0092fd6de43031a4d
Stefano Tommasi <86169@sistemas.frc.utn.edu.ar>
2024-04-28 04:21:49 -0300

    TP3 casi terminado

--- TP3/logica.py
+++ TP3/logica.py
@@ -1,17 +1,42 @@
 from generador_numeros import rnd
 
-INGRESOS = 4000
-COSTO = 2400
-COSTO_OBRERO = 30
-CANTIDAD_MINIMA_OBREROS_PRESENTES = 20
 
-ausentismo = {
-    0: 0.36,
-    1: 0.74,
-    2: 0.93,
-    3: 0.99,
-    4: 1,
-}
+class Parametros:
+    def __init__(self, ingresos_venta, costo_venta, remuneracion_obrero):
+        self.ingresos_venta = ingresos_venta
+        self.costo_venta = costo_venta
+        self.remuneracion_obrero = remuneracion_obrero
+        self.probabilidades = []
+
+    def getIngreso(self):
+        return self.ingresos_venta
+
+    def getCostoVenta(self):
+        return self.costo_venta
+
+    def getRemuneracionObrero(self):
+        return self.remuneracion_obrero
+
+    def setIngreso(self, ingreso):
+        self.ingresos_venta = ingreso
+
+    def setCostoVenta(self, costo):
+        self.costo_venta = costo
+
+    def setRemuneracionObrero(self, remuneracion):
+        self.remuneracion_obrero = remuneracion
+
+    def setear_probabilidades(self, frecuencias):
+        frecuencias_acumuladas = sum(frecuencias)
+        probabilidad_acumulada = 0
+
+        for ausentes, frecuencia in enumerate(frecuencias):
+            probabilidad = round((frecuencia / frecuencias_acumuladas), 4)
+            probabilidad_acumulada += probabilidad
+            self.probabilidades.append((ausentes, probabilidad_acumulada))
+
+    def getProbabilidad(self):
+        return self.probabilidades
 
 
 class Simulacion:
@@ -25,14 +50,57 @@ class Simulacion:
         self.beneficio = beneficio
         self.beneficio_acumulado = beneficio_acumulado
 
+    def get_dia(self):
+        return self.dia
 
-def encontrar_ausentimo(rnd_obrero):
-    for ausentes, probabilidad_acumulada in ausentismo.items():
+    def set_dia(self, dia):
+        self.dia = dia
+
+    def get_rnd_obrero(self):
+        return self.rnd_obrero
+
+    def set_rnd_obrero(self, rnd_obrero):
+        self.rnd_obrero = rnd_obrero
+
+    def get_obreros_ausentes(self):
+        return self.obreros_ausentes
+
+    def set_obreros_ausentes(self, obreros_ausentes):
+        self.obreros_ausentes = obreros_ausentes
+
+    def get_obreros_presentes(self):
+        return self.obreros_presentes
+
+    def set_obreros_presentes(self, obreros_presentes):
+        self.obreros_presentes = obreros_presentes
+
+    def get_planta_operable(self):
+        return self.planta_operable
+
+    def set_planta_operable(self, booleano):
+        self.planta_operable = booleano
+
+    def get_beneficio(self):
+        return self.beneficio
+
+    def set_beneficio(self, beneficio):
+        self.beneficio = beneficio
+
+    def get_beneficio_acumulado(self):
+        return self.beneficio_acumulado
+
+    def set_beneficio_acumulado(self, beneficio_acumulado):
+        self.beneficio_acumulado = beneficio_acumulado
+
+
+def encontrar_ausentismo(rnd_obrero, objeto_parametros):
+    probabilidades = objeto_parametros.getProbabilidad()
+
+    for ausentes, probabilidad_acumulada in probabilidades:
         if rnd_obrero < probabilidad_acumulada:
             return ausentes
-
-    return 4
-
+        
+    return probabilidades[-1][0]
 
 def es_operable(obreros_presentes):
     if obreros_presentes >= 20:
@@ -40,24 +108,28 @@ def es_operable(obreros_presentes):
     return "No"
 
 
-def simulacion(cant_dias, cant_obreros, rango_desde, rango_hasta):
+def simulacion(cant_dias, cant_obreros, rango_desde, rango_hasta, objeto_parametros):
     lista_simulacion = []
     beneficio_acumulado = 0
 
     for i in range(cant_dias):
-        dia = i+1
+        dia = i + 1
         rnd_obrero = round(rnd(), 4)
-        obreros_ausentes = encontrar_ausentimo(rnd_obrero)
+
+        obreros_ausentes = encontrar_ausentismo(rnd_obrero, objeto_parametros)
         obreros_presentes = cant_obreros - obreros_ausentes
+
         planta_operable = es_operable(obreros_presentes)
+
         ingresos_por_venta = 0
         costos_por_venta = 0
 
         if planta_operable == "Si":
-            ingresos_por_venta = INGRESOS
-            costos_por_venta = COSTO
+            ingresos_por_venta = objeto_parametros.getIngreso()
+            costos_por_venta = objeto_parametros.getCostoVenta()
 
-        sueldos_obreros = cant_obreros * COSTO_OBRERO
+        sueldos_obreros = cant_obreros * objeto_parametros.getRemuneracionObrero()
+
         beneficio = ingresos_por_venta - costos_por_venta - sueldos_obreros
         beneficio_acumulado += beneficio
 
@@ -79,6 +151,6 @@ def simulacion(cant_dias, cant_obreros, rango_desde, rango_hasta):
         return lista_simulacion
 
     if rango_hasta == ultimo_dia:
-        return lista_simulacion[rango_desde-1:rango_hasta]
+        return lista_simulacion[rango_desde - 1:rango_hasta]
 
-    return lista_simulacion[rango_desde-1:rango_hasta] + [lista_simulacion[-1]]
+    return lista_simulacion[rango_desde - 1:rango_hasta] + [lista_simulacion[-1]]
--- TP3/ui.py
+++ TP3/ui.py
@@ -1,11 +1,12 @@
 import tkinter as tk
 from tkinter import ttk, messagebox
-from logica import simulacion
+from logica import simulacion, Parametros
 
 
 class MainWindow(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
+
         self.title("Ingreso de iteraciones")
         self.geometry("300x200")
 
@@ -29,7 +30,7 @@ class MainWindow(tk.Toplevel):
             dias = int(self.tamaño_entry.get())
             obreros = int(self.obreros_combobox.get())
             if 1 <= dias <= 100000:
-                self.destroy()
+                self.master.withdraw()
                 SimulacionWindow(self.master, dias, obreros)
             else:
                 messagebox.showerror("Error", "La cantidad de días de la simulación tiene que estar entre 1 y 100.000")
@@ -41,26 +42,88 @@ class SimulacionWindow(tk.Toplevel):
     def __init__(self, parent, dias, obreros):
         super().__init__(parent)
         self.title("Simulación")
-        self.geometry("1450x400")
+        self.geometry("1600x900")
 
-        self.label_dia_desde = tk.Label(self, text="Ingrese desde que día de la simulación quiere observar: ")
-        self.label_dia_desde.pack()
+
+        self.label_ingresos_por_venta = tk.Label(self, text="Ingresos por venta:")
+        self.label_ingresos_por_venta.place(relx=0.05, rely=0.05)
+
+        self.ingresos_por_venta_entry = tk.Entry(self)
+        self.ingresos_por_venta_entry.place(relx=0.05, rely=0.1)
+
+        self.label_costos_por_venta = tk.Label(self, text="Costos por venta:")
+        self.label_costos_por_venta.place(relx=0.05, rely=0.2)
+
+        self.costos_por_venta_entry = tk.Entry(self)
+        self.costos_por_venta_entry.place(relx=0.05, rely=0.25)
+
+        self.label_remuneracion_obrero = tk.Label(self, text="Costos de remuneración por obrero:")
+        self.label_remuneracion_obrero.place(relx=0.05, rely=0.35)
+
+        self.remuneracion_obrero_entry = tk.Entry(self)
+        self.remuneracion_obrero_entry.place(relx=0.05, rely=0.4)
+
+        # Labels y entries de día desde y día hasta (derecha arriba)
+        self.label_dia_desde = tk.Label(self, text="Desde qué día:")
+        self.label_dia_desde.place(relx=0.75, rely=0.05)
 
         self.rango_desde_entry = tk.Entry(self)
-        self.rango_desde_entry.pack()
+        self.rango_desde_entry.place(relx=0.75, rely=0.1)
 
-        self.label_dia_hasta = tk.Label(self, text="Ingrese hasta que día de la simulación quiere observar: ")
-        self.label_dia_hasta.pack()
+        self.label_dia_hasta = tk.Label(self, text="Hasta qué día:")
+        self.label_dia_hasta.place(relx=0.75, rely=0.15)
 
         self.rango_hasta_entry = tk.Entry(self)
-        self.rango_hasta_entry.pack()
+        self.rango_hasta_entry.place(relx=0.75, rely=0.20)
+        
 
-        self.ok_button = tk.Button(self, text="OK", command=lambda: mostrar_datos(self, dias, obreros))
-        self.ok_button.pack()
-
+        # Labels y entries de obreros (medio arriba)
+        self.label_frecuencia = tk.Label(self, text="Cantidad de veces que se ausentaron los obreros:")
+        self.label_frecuencia.place(relx=0.4, rely=0.05)
+        
+        self.label_frecuencia_obrero_cero = tk.Label(self, text = "0 obreros")
+        self.label_frecuencia_obrero_cero.place(relx = 0.4, rely=0.1)
+        
+        self.frecuencia_obrero_cero_entry = tk.Entry(self)
+        self.frecuencia_obrero_cero_entry.place(relx = 0.47, rely=0.1)
+        
+        self.label_frecuencia_obrero_uno = tk.Label(self, text = "1 obrero")
+        self.label_frecuencia_obrero_uno.place(relx = 0.4, rely=0.15)
+        
+        self.frecuencia_obrero_uno_entry = tk.Entry(self)
+        self.frecuencia_obrero_uno_entry.place(relx = 0.47, rely=0.15)
+        
+        self.label_frecuencia_obrero_dos = tk.Label(self, text = "2 obreros")
+        self.label_frecuencia_obrero_dos.place(relx = 0.4, rely=0.2)
+        
+        self.frecuencia_obrero_dos_entry = tk.Entry(self)
+        self.frecuencia_obrero_dos_entry.place(relx = 0.47, rely=0.2)
+        
+        self.label_frecuencia_obrero_tres = tk.Label(self, text = "3 obreros")
+        self.label_frecuencia_obrero_tres.place(relx = 0.4, rely=0.25)
+        
+        self.frecuencia_obrero_tres_entry = tk.Entry(self)
+        self.frecuencia_obrero_tres_entry.place(relx = 0.47, rely=0.25)
+        
+        self.label_frecuencia_obrero_cuatro = tk.Label(self, text = "4 obreros")
+        self.label_frecuencia_obrero_cuatro.place(relx = 0.4, rely=0.3)
+        
+        self.frecuencia_obrero_cuatro_entry = tk.Entry(self)
+        self.frecuencia_obrero_cuatro_entry.place(relx = 0.47, rely=0.3)
+        
+        self.label_frecuencia_obrero_cinco = tk.Label(self, text = "5 o más obreros")
+        self.label_frecuencia_obrero_cinco.place(relx = 0.4, rely=0.35)
+        
+        self.frecuencia_obrero_cinco_entry = tk.Entry(self)
+        self.frecuencia_obrero_cinco_entry.place(relx = 0.47, rely=0.35)
+        
+        self.ok_button = tk.Button(self, text="OK", command=lambda: mostrar_datos(self, dias, obreros), width=20)
+        self.ok_button.place(relx=0.45, rely=0.5)
+        
         self.treeview = ttk.Treeview(self, columns=(
             "dia", "rnd_obrero", "obreros_ausentes", "obreros_presentes", "planta_operable", "beneficio",
             "beneficio_acumulado"), show="headings")
+        
         self.treeview.heading("dia", text="Día")
         self.treeview.heading("rnd_obrero", text="RND Obrero")
         self.treeview.heading("obreros_ausentes", text="Obreros Ausentes")
@@ -68,8 +131,17 @@ class SimulacionWindow(tk.Toplevel):
         self.treeview.heading("planta_operable", text="Planta Operable")
         self.treeview.heading("beneficio", text="Beneficio $")
         self.treeview.heading("beneficio_acumulado", text="Beneficio Acumulado $")
-        self.treeview.pack(side="left", fill="both", expand=True)
-
+        
+        self.treeview.column("dia", width=5, anchor="center")
+        self.treeview.column("rnd_obrero", width=5, anchor="center")
+        self.treeview.column("obreros_ausentes", width=5, anchor="center")
+        self.treeview.column("obreros_presentes", width=5, anchor="center")
+        self.treeview.column("planta_operable", width=5, anchor="center")
+        self.treeview.column("beneficio", width=15, anchor="center")
+        self.treeview.column("beneficio_acumulado", width=15, anchor="center")
+        
+        self.treeview.place(relx=0.2, rely=0.6, relwidth=0.6, relheight=0.3)
+        
         scrollbar = tk.Scrollbar(self, orient="vertical", command=self.treeview.yview)
         scrollbar.pack(side="right", fill="y")
         self.treeview.configure(yscrollcommand=scrollbar.set)
@@ -78,17 +150,47 @@ class SimulacionWindow(tk.Toplevel):
             try:
                 rango_desde = int(self.rango_desde_entry.get())
                 rango_hasta = int(self.rango_hasta_entry.get())
-
+                ingresos_venta = float(self.ingresos_por_venta_entry.get())
+                costos_venta = float(self.costos_por_venta_entry.get())
+                remuneracion_obrero = float(self.remuneracion_obrero_entry.get())
+                
+                frecuencias = [self.frecuencia_obrero_cero_entry.get(),
+                               self.frecuencia_obrero_uno_entry.get(),
+                               self.frecuencia_obrero_dos_entry.get(),
+                               self.frecuencia_obrero_tres_entry.get(),
+                               self.frecuencia_obrero_cuatro_entry.get(),
+                               self.frecuencia_obrero_cinco_entry.get(),]
+                
                 if rango_hasta < rango_desde or rango_desde > dias or rango_desde < 0 or rango_hasta > dias:
                     messagebox.showerror("Error", "Rango invalido")
-                else:
-                    self.treeview.delete(*self.treeview.get_children())  # Limpiar la tabla
-                    lista = simulacion(dias, obreros, rango_desde, rango_hasta)
-                    for i, obj in enumerate(lista, start=0):
-                        self.treeview.insert("", "end", text=str(i), values=(
-                            obj.dia, obj.rnd_obrero, obj.obreros_ausentes, obj.obreros_presentes, obj.planta_operable,
-                            '$' + str(obj.beneficio), '$' + str(obj.beneficio_acumulado)))
-
+                    
+                if ingresos_venta < 0 or costos_venta < 0 or remuneracion_obrero < 0:
+                    messagebox.showerror("Error", "No se pueden ingresar valores negativos")
+                
+                lista_frecuencias = list(map(int, frecuencias))
+                
+                for i in lista_frecuencias:
+                    if i <= 0:
+                        messagebox.showerror("Error", "Las frecuencias deben ser positivas y enteras")
+                
+                objeto_parametros = Parametros(ingresos_venta = ingresos_venta, 
+                                               costo_venta = costos_venta, 
+                                               remuneracion_obrero = remuneracion_obrero)
+                
+                objeto_parametros.setear_probabilidades(lista_frecuencias)
+                
+                self.treeview.delete(*self.treeview.get_children())  # Limpiar la tabla
+                
+                lista = simulacion(dias, obreros, rango_desde, rango_hasta, objeto_parametros)
+                
+                for i, obj in enumerate(lista, start=0):
+                    self.treeview.insert("", "end", text=str(i), values=(
+                        obj.get_dia(), obj.get_rnd_obrero(), obj.get_obreros_ausentes(), obj.get_obreros_presentes(), 
+                        obj.get_planta_operable(), '$' + str(obj.get_beneficio()), '$' + str(obj.get_beneficio_acumulado())))
+                    
+                self.label_beneficio_total = tk.Label(self, text=f"Beneficio total: ${lista[-1].get_beneficio_acumulado()}")
+                self.label_beneficio_total.place(relx=0.45, rely=0.9)
+                
             except ValueError:
                 messagebox.showerror("Error", "Ingreso inválido")
 
